bee430e941ccc59c89791f9b4aa9c61f36733244
XhmikosR <xhmikosr@users.sourceforge.net>
2014-03-17 07:42:05 +0200

    Remove unused variables.

--- docs/assets/js/_src/application.js
+++ docs/assets/js/_src/application.js
@@ -122,7 +122,7 @@
     var htmlBridge = $('#global-zeroclipboard-html-bridge')
 
     // Handlers for ZeroClipboard
-    zeroClipboard.on('load', function(client) {
+    zeroClipboard.on('load', function () {
       htmlBridge
         .data('placement', 'top')
         .attr('title', 'Copy to clipboard')
@@ -130,13 +130,13 @@
     })
 
     // Copy to clipboard
-    zeroClipboard.on('dataRequested', function(client) {
+    zeroClipboard.on('dataRequested', function (client) {
       var highlight = $(this).parent().nextAll('.highlight').first()
       client.setText(highlight.text())
     })
 
     // Notify copy success and reset tooltip title
-    zeroClipboard.on('complete', function(client) {
+    zeroClipboard.on('complete', function () {
       htmlBridge
         .attr('title', 'Copied!')
         .tooltip('fixTitle')
@@ -146,7 +146,7 @@
     })
 
     // Notify copy failure
-    zeroClipboard.on('noflash wrongflash', function(client) {
+    zeroClipboard.on('noflash wrongflash', function () {
       htmlBridge
         .attr('title', 'Flash required')
         .tooltip('fixTitle')
